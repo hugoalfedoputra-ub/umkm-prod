b013a025554f65dab17478233d9ace952c806013
Kroja-Nk <166620425+rizkyaqil04@users.noreply.github.com>
2024-06-09 19:13:10 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $products = Product::latest()->paginate(6)->withQueryString();
-        return view('home', compact('products'));
+
+        if (Auth::check() && Auth::user()->userrole == 'admin') {
+            return redirect()->route('admin.dashboard');
+        } else {
+            $products = Product::latest()->paginate(6)->withQueryString();
+            return view('home', compact('products'));
+        }
     }
 }
